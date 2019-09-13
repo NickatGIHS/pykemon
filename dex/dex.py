8119330b7831f3a90c023337f3d8e6a4e8b69641
Nick LAITY <180317@CURRIC.GIHS.SA.EDU.AU>
2019-09-13 17:09:16 +0930

    The code for Complete the 'dex
    Starting it, anyway

---
+++ dex/dex.py
@@ -0,0 +1,15 @@
+# ==============================
+# | PYKEMON: COMPLETE THE 'DEX |
+# ------------------------------
+# |       by NickatGIHS        |
+# ==============================
+
+# Files are located (in relation to this):
+#
+# Unobtainable: unobt-dual.csv
+# Game-specific: game-specif\\(#).csv
+# Mythical/Event: ..\\myth.csv
+# All Pokemon: ..\\pokemon.csv
+f-unob = open("unobt-dual.csv")
+f-myth = open("..\\myth.csv")
+f-poke = open("..\\pokemon.csv")
